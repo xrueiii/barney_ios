f09fad138fbd376c03a7860b6a16a40978293c8a
howard921109 <howard921109@gmail.com>
2024-12-09 18:40:52 +0800

    ReserveViewWithTime

--- barney_ios/ReserveView.swift
+++ barney_ios/ReserveView.swift
@@ -3,18 +3,34 @@ import SwiftUI
 struct ReserveView: View {
     @State private var selectedDate = Date()
     @State private var numberOfPeople = 1
+    @State private var selectedTime = "12:00 PM" // Default selected time
     @State private var branches: [Branch] = [] // List of branches available for reservation
     @State private var selectedBranch: Branch? = nil
     @State private var showBranchList = false
     @State private var isLoading = false // No need to show loading animation with fake data
 
+    let availableTimes = [
+        "12:00 AM", "12:15 AM", "12:30 AM", "12:45 AM", "1:00 AM", "1:15 AM", "1:30 AM", "1:45 AM",
+        "2:00 AM", "2:15 AM", "2:30 AM", "2:45 AM", "3:00 AM", "3:15 AM", "3:30 AM", "3:45 AM",
+        "4:00 AM", "4:15 AM", "4:30 AM", "4:45 AM", "5:00 AM", "5:15 AM", "5:30 AM", "5:45 AM",
+        "6:00 AM", "6:15 AM", "6:30 AM", "6:45 AM", "7:00 AM", "7:15 AM", "7:30 AM", "7:45 AM",
+        "8:00 AM", "8:15 AM", "8:30 AM", "8:45 AM", "9:00 AM", "9:15 AM", "9:30 AM", "9:45 AM",
+        "10:00 AM", "10:15 AM", "10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM",
+        "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "1:00 PM", "1:15 PM", "1:30 PM", "1:45 PM",
+        "2:00 PM", "2:15 PM", "2:30 PM", "2:45 PM", "3:00 PM", "3:15 PM", "3:30 PM", "3:45 PM",
+        "4:00 PM", "4:15 PM", "4:30 PM", "4:45 PM", "5:00 PM", "5:15 PM", "5:30 PM", "5:45 PM",
+        "6:00 PM", "6:15 PM", "6:30 PM", "6:45 PM", "7:00 PM", "7:15 PM", "7:30 PM", "7:45 PM",
+        "8:00 PM", "8:15 PM", "8:30 PM", "8:45 PM", "9:00 PM", "9:15 PM", "9:30 PM", "9:45 PM",
+        "10:00 PM", "10:15 PM", "10:30 PM", "10:45 PM", "11:00 PM", "11:15 PM", "11:30 PM", "11:45 PM"
+    ]
+
     var body: some View {
         NavigationStack {
             ZStack {
-                VStack(spacing: 2) {
-                    // If branch list is not shown, show calendar and number of people
+                VStack(spacing: 20) {
+                    // If branch list is not shown, show calendar, time picker, and number of people
                     if !showBranchList {
-                         //Logo Image
+                        // Logo Image
                         Image("logo")
                             .resizable()
                             .scaledToFill()
@@ -22,18 +38,31 @@ struct ReserveView: View {
                             .clipped()
 
                         // Select Date
-                        VStack(alignment: .leading, spacing: 1) {
-                            Text("Choose a Date")
-                                .font(.headline)
+                        VStack(alignment: .leading, spacing: 10) {
+//                            Text("Choose a Date")
+//                                .font(.headline)
                             DatePicker("Reservation Date", selection: $selectedDate, displayedComponents: .date)
                                 .datePickerStyle(GraphicalDatePickerStyle())
                                 .padding()
                         }
 
+                        // Select Time
+                        VStack(alignment: .leading, spacing: 10) {
+//                            Text("Choose a Time")
+//                                .font(.headline)
+                            Picker("Select Time", selection: $selectedTime) {
+                                ForEach(availableTimes, id: \.self) { time in
+                                    Text(time).tag(time)
+                                }
+                            }
+                            .pickerStyle(WheelPickerStyle())
+                            .frame(height: 100)
+                        }
+
                         // Select Number of People
                         VStack(alignment: .leading, spacing: 10) {
-                            Text("Number of People")
-                                .font(.headline)
+//                            Text("Number of People")
+//                                .font(.headline)
                             Stepper(value: $numberOfPeople, in: 1...20) {
                                 Text("\(numberOfPeople) people")
                                     .font(.body)
@@ -54,7 +83,7 @@ struct ReserveView: View {
                                 .foregroundColor(.white)
                                 .cornerRadius(10)
                         }
-                        
+
                     } else {
                         // Display Branches
                         if isLoading {
@@ -166,9 +195,10 @@ struct ReserveView: View {
     // Make reservation
     func makeReservation() {
         guard let branch = selectedBranch else { return }
-        print("Reservation confirmed for \(branch.name) on \(selectedDate) for \(numberOfPeople) people.")
+        print("Reservation confirmed for \(branch.name) on \(selectedDate) at \(selectedTime) for \(numberOfPeople) people.")
     }
 }
+
 
 struct ReserveBranchCard: View {
     var branchImage: String
